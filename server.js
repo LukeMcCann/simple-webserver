617f457b0a30b2bea09e72af26328cac03348e7e
LukeMcCann <developer-lukemccann@outlook.com>
2021-05-23 21:38:40 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+require('dotenv').config();
+const PORT = process.env.PORT;
+const express = require('express');
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+});
+
